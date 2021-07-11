3ec14fb60e9bac3fcc9f10d6402a13b7dda5c7d0
zedword <87199325+zedword@users.noreply.github.com>
2021-07-11 15:53:13 -0300

    Create groups.py

---
+++ groups.py
@@ -0,0 +1,45 @@
+from keys import keys
+from libqtile.config import Group, Match, Key
+from libqtile.lazy import lazy
+from libqtile import layout
+
+mod = "mod4"
+
+group_names = [("", {'layout': 'columns'}),
+               ("", {'layout': 'columns'}),
+               ("", {'layout': 'columns'}),
+               ("", {'layout': 'columns'}),
+               ("", {'layout': 'columns'}),
+               ("", {'layout': 'columns', 'matches':[Match(wm_class=['keepassxc'])]}),]
+
+groups = [Group(name, **kwargs) for name, kwargs in group_names]
+
+for i, (name, kwargs) in enumerate(group_names, 1):
+    keys.append(Key([mod], str(i), lazy.group[name].toscreen()))        # Switch to another group
+    keys.append(Key([mod, "shift"], str(i), lazy.window.togroup(name))) # Send current window to another group
+
+def init_layout_theme():
+    return {'margin':10,
+            'border_width':1,
+            'border_focus': '#bd93f9',
+            'border_normal': '#4c566a',
+            }
+
+layout_theme = init_layout_theme()
+
+layouts = [
+   layout.Columns(**layout_theme),
+    layout.MonadTall(**layout_theme),
+    layout.Max(**layout_theme),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    layout.MonadWide(**layout_theme),
+    layout.Floating(**layout_theme),
+    layout.RatioTile(**layout_theme),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
